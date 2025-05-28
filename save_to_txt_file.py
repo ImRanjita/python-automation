d00795e33ccea0178ca5a6426c69e668a03eaf39
karthikhegde52 <karthikhegde52@gmail.com>
2025-05-28 23:36:10 +0530

    udemy course work

--- save_to_txt_file.py
+++ save_to_txt_file.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
+from datetime import datetime
 import time
 service = Service('C:\\Users\\Karthik\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe')
 def get_driver():
@@ -18,24 +19,43 @@ def get_driver():
     return driver
 
 
-#def clean_text(text):
-  #  """extract only the value from the text"""
-    #output = float(text.split(": ")[1])
-   # return output
+def clean_text(text):
+    """extract only the value from the text"""
+    output = float(text.split(": ")[1])
+    return output
+
 
 
 def main():
 
 
     driver = get_driver()
-    driver.find_element(by="id",value ="id_username").send_keys("automated")
+    ""driver.find_element(by="id",value ="id_username").send_keys("automated")
     time.sleep(2)
     driver.find_element(by="id",value ="id_password").send_keys("automatedautomated" + Keys.RETURN)
     print(driver.current_url)
     time.sleep(2)
     driver.find_element(by="xpath", value = "/html/body/nav/div/a").click()
     print(driver.current_url)
-##    driver.find_element(by="xpath", value = "").click()
+    element = driver.find_element(by="xpath", value="//*[@id='displaytimer']")
+    print(element)
+    cleaned_line =  clean_text(element)
+   # save_output_to_file()
+
+
+def save_output_to_file() :
+    # Get current timestamp
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    # Create file name with date-time
+    file_name = f"web_output_{timestamp}.txt"
+
+    # Save to text file
+    with open(file_name, "w", encoding="utf-8") as file:
+        file.write(cleaned_line)
+
+    print(f"Saved: {file_name}")
+    return
+
 
 print(main())
 
